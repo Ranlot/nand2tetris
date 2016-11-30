7705613721b5636885083e939056d0d2be3f9725
tami_boue <spamoftami@gmail.com>
2016-11-30 20:35:31 +0200

    we start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static int countWords(String instruction) {
+        return instruction.split(" ").length;
+    }
 
-        System.out.println("hi");
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("src/main/resources/Add.asm");
+
+        try(Stream<String> streamOfInstructions = Files.lines(path)) {
+            List<Integer> numberOfWords = streamOfInstructions.map(Main::countWords).collect(Collectors.toList());
+            System.out.println(numberOfWords);
+
+        }
 
     }
 
