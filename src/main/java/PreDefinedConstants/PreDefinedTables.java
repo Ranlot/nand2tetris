85085b498013f437787ef9f2146f1f22c2bcb2bd
Laurent <ranlot75@gmail.com>
2016-12-14 20:34:09 +0200

    fixed the table of compute instructions

--- src/main/java/PreDefinedConstants/PreDefinedTables.java
+++ src/main/java/PreDefinedConstants/PreDefinedTables.java
@@ -1,8 +1,6 @@
 package PreDefinedConstants;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -56,51 +54,60 @@ abstract public class PreDefinedTables {
             put("KBD", "24576");
     }};
 
-    public static final Multimap<String, String> computeTableContent = new ImmutableMultimap.Builder<String, String>()
+    public static final Map<String, String> computeTableContent = ImmutableMap.<String, String>builder()
             .put("0", "101010")
             .put("1", "111111")
             .put("-1", "111010")
+
             .put("D", "001100")
             .put("A", "110000")
             .put("M", "110000")
+
             .put("!D", "001101")
             .put("!A", "110001")
             .put("!M", "110001")
+
             .put("-D", "001111")
             .put("-A", "110011")
             .put("-M", "110011")
+
             .put("D+1", "011111")
             .put("1+D", "011111") //same as D+1
-            .put("1+A", "110111")
-            .put("A+1", "110111") //same as 1+A
-            .put("1+M", "110111")
-            .put("M+1", "110111") //same as 1+M
-            .put("1-D", "001110")
-            .put("D-1", "001110") //same as 1-D
-            .put("1-A", "110010")
-            .put("A-1", "110010") //same as 1-A
-            .put("1-M", "110010")
-            .put("M-1", "110010") //same as 1-M
+
+            .put("A+1", "110111")
+            .put("1+A", "110111") //same as A+1
+
+            .put("M+1", "110111")
+            .put("1+M", "110111") //same as 1+M
+
+            .put("D-1", "001110")
+            .put("A-1", "110010")
+            .put("M-1", "110010")
+
             .put("D+A", "000010")
             .put("A+D", "000010") //same as D+A
+
             .put("D+M", "000010")
             .put("M+D", "000010") //same as D+M
+
             .put("D-A", "010011")
-            .put("A-D", "010011") //same as D-A
             .put("D-M", "010011")
-            .put("M-D", "010011") //same as D-M
+
             .put("A-D", "000111")
-            .put("D-A", "000111") //same as A-D
             .put("M-D", "000111")
-            .put("D-M", "000111") //same as M-D
+
             .put("D&A", "000000")
             .put("A&D", "000000") //same as A&D
+
             .put("D&M", "000000")
             .put("M&D", "000000") //same as D&M
+
             .put("D|A", "010101")
             .put("A|D", "010101") //same as D|A
+
             .put("D|M", "010101")
             .put("M|D","010101")  //same as M|D
+
 	        .build();
 
     /*public static Map<SymbolTableNames, Map<String, String>> getPreDefinedMaps() {
