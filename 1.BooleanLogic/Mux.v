83282b85e7d9a6e101e800bcdd7d618e81b68f03
Laurent <ranlot75@gmail.com>
2016-12-16 23:01:10 +0200

    new logic gates

---
+++ 1.BooleanLogic/Mux.v
@@ -0,0 +1,10 @@
+CHIP Mux {
+    IN a, b, sel;
+    OUT out;
+
+    PARTS:
+    Nand(a=b, b=sel, out=n1);
+    Nand(a=sel, b=sel, out=n2);
+    Nand(a=n2, b=a, out=n3);
+    Nand(a=n1, b=n3, out=out);
+}
